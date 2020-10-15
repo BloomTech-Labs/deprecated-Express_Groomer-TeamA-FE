66cfbf4badb95b69fa63bd1ea96eed9030d36276
MLucky518 <56007160+MLucky518@users.noreply.github.com>
2020-10-15 02:22:11 -0400

    small syntax change in reducer

--- src/state/reducers/appReducer.js
+++ src/state/reducers/appReducer.js
@@ -10,7 +10,7 @@ export const appReducer = (state = initialState, action) => {
       console.log(action.payload);
       return {
         ...state,
-        profiles: [action.payload],
+        profiles: action.payload,
       };
 
     default:
