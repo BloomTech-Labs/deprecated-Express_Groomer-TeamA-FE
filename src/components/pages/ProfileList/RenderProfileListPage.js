8e666778559dfda147d5883786fb2d2d5576068a
MLucky518 <56007160+MLucky518@users.noreply.github.com>
2020-10-27 21:22:50 -0400

    added npm packacge for react eslint

--- src/components/pages/ProfileList/RenderProfileListPage.js
+++ src/components/pages/ProfileList/RenderProfileListPage.js
@@ -3,8 +3,9 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import SearchForm from '../Search/SearchForm';
 import { connect } from 'react-redux';
-import { Image, Card } from 'antd';
-
+import { Image, Card, Avatar, Rate } from 'antd';
+import './profileList.scss';
+import { ScissorOutlined } from '@ant-design/icons';
 const { Meta } = Card;
 
 function RenderProfileListPage(props) {
@@ -30,12 +31,36 @@ function RenderProfileListPage(props) {
         <Link to="/">Home</Link>
       </p>
       <SearchForm value={searched} handleChange={handleChange} />
-      {filtered.map(item => (
-        <Card key={item.id}>
-          <Image src={item.avatarUrl} alt={item.name} />
-          <Meta title={item.name} description={'user or groomer'}></Meta>
-        </Card>
-      ))}
+      <div className="profile-list">
+        {filtered.map(item => (
+          <Card
+            className="profile-card spin"
+            key={item.id}
+            cover={
+              <img
+                className="card-image"
+                src={item.avatarUrl}
+                alt={item.name}
+              />
+            }
+          >
+            <Meta
+              className="card-data"
+              title={item.name}
+              description={'user or groomer'}
+            ></Meta>
+            <br />
+            Rating:
+            <br />
+            <Rate
+              character={<ScissorOutlined />}
+              style={{ 'margin-top': '5%', color: 'black' }}
+              disabled
+              defaultValue={5}
+            />
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
@@ -45,7 +70,7 @@ const mapStateToProps = state => {
     profiles: state.profiles,
   };
 };
-// Line below i believe is causing prop type errors
+
 export default connect(mapStateToProps, {})(RenderProfileListPage);
 
 // Don't forget your prop types! It will save you a lot of debugging headache as you add more features.
