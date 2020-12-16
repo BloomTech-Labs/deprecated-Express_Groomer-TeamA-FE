5008c91ccd0a2c7aeb002cc6bd37b087b82bee59
Russell Smith <russellsmith@MacBook-Pro.local>
2020-12-16 17:47:35 -0500

    added functionality to edit user information

--- src/components/pages/Profile/RenderCustomerProfile.js
+++ src/components/pages/Profile/RenderCustomerProfile.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import { Row, Col, Avatar, Button } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
+import CustomerInfo from './CustomerInfo';
+import CustomerEditInfo from './CustomerEditInfo';
 import './profile.css';
 
 const RenderCustomerProfile = ({ userInfo }) => {
+  const [displayUserInfoInputs, toggleUserInfoInputs] = useState(false);
+  const [userFormData, setUserFormData] = useState({
+    name: '',
+    email: '',
+  });
+
+  useEffect(() => {
+    setUserFormData({
+      name: userInfo.name,
+      email: userInfo.email,
+    });
+  }, [userInfo]);
+
+  const onChange = e => {
+    setUserFormData({ ...userFormData, [e.target.name]: e.target.value });
+  };
+
+  // Edit User Info Submit Function
+  const onSubmit = e => {
+    e.preventDefault();
+    setUserFormData(userFormData);
+  };
+
+  const { name, email } = userFormData;
+
   return (
     <div>
       {userInfo && (
         <div className="profile-container">
           <Row gutter={[16, 16]}>
             <Col xs={{ span: 24 }} sm={{ span: 8 }} md={{ span: 8 }}>
-              <div className="customer-info">
-                <Avatar size={84} icon={<UserOutlined />} />
-                <p>Username: User1</p>
-                <p>Email: user@email.com</p>
-                <Button type="primary" size={'large'}>
-                  Edit
-                </Button>
-              </div>
+              {!displayUserInfoInputs ? (
+                <CustomerInfo
+                  name={name}
+                  email={email}
+                  toggleUserInfoInputs={toggleUserInfoInputs}
+                  displayUserInfoInputs={displayUserInfoInputs}
+                />
+              ) : (
+                <CustomerEditInfo
+                  name={name}
+                  email={email}
+                  saveChanges={onSubmit}
+                  updateForm={onChange}
+                  toggleUserInfoInputs={toggleUserInfoInputs}
+                  displayUserInfoInputs={displayUserInfoInputs}
+                />
+              )}
             </Col>
             <Col xs={{ span: 24 }} sm={{ span: 16 }} md={{ span: 16 }}>
               <div className="upcoming-appointments">
@@ -72,7 +107,7 @@ const RenderCustomerProfile = ({ userInfo }) => {
               </Col>
               <Col xs={{ span: 24 }} sm={{ span: 8 }} md={{ span: 8 }}>
                 <div className="add-pets">
-                  <i class="fas fa-plus"></i>
+                  <i className="fas fa-plus"></i>
                 </div>
               </Col>
             </Row>
