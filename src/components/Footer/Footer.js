691ba2e829f9efc23941b1da984ab72ecb672742
cdsuddreth <cdsuddreth@gmail.com>
2021-01-13 22:03:47 -0500

    changed logo and branding to reflect name change

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {FooterContainer, FooterName} from './footerStyles';
-import logo from '../../assets/9f5a8b6a-18d6-4a28-9e98-1eee8c05b05a_200x200.png';
+import logo from '../../assets/Pet Express.png';
 
 function Footer() {
   return (
     <FooterContainer>
       <img src={logo} alt="cat and dog together" />
-      <FooterName>Pet-XPress © 2020</FooterName>
+      <FooterName>Pet Express © 2021</FooterName>
       <div className="footer-icons">
         <i className="fab fa-facebook footer-icon"></i>
         <i className="fab fa-twitter footer-icon"></i>
